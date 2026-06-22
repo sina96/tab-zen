9305c1776dd7145bb65a289a0028d7c8c3c1ce27
sina96 <sina@sinabastani.dev>
2026-06-23 00:46:37 +0200

    Add Stage 2 tab counter badge

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,43 @@
+const BADGE_BACKGROUND = "#2563eb";
+
+const updateTabCountBadge = async (): Promise<void> => {
+  const tabs = await chrome.tabs.query({});
+  const tabCount = tabs.length;
+
+  await chrome.action.setBadgeText({
+    text: tabCount > 0 ? String(tabCount) : ""
+  });
+  await chrome.action.setBadgeBackgroundColor({ color: BADGE_BACKGROUND });
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-  void chrome.action.setBadgeBackgroundColor({ color: "#2563eb" });
+  void updateTabCountBadge();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.tabs.onCreated.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.tabs.onRemoved.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.tabs.onAttached.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.tabs.onDetached.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.windows.onCreated.addListener(() => {
+  void updateTabCountBadge();
+});
+
+chrome.windows.onRemoved.addListener(() => {
+  void updateTabCountBadge();
 });
